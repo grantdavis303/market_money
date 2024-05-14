7023ee34975b461efe2b320137d02922690beee5
Grant Davis <grantdavis303@gmail.com>
2024-05-14 15:09:50 -0600

    Create Tests for US3 - happy and sad

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -6,7 +6,7 @@ describe "Markets API" do
   it "sends back all markets" do
     create_list(:market, 10)
 
-    get '/api/v0/markets'
+    get "/api/v0/markets"
     parsed_json = JSON.parse(response.body, symbolize_names: true)
     markets = parsed_json[:data]
 
@@ -39,5 +39,85 @@ describe "Markets API" do
       expect(market[:attributes][:lon]).to be_a(String)
       expect(market[:attributes][:vendor_count]).to be_a(Integer)
     end
+  end
+
+  # User Story 2 - Happy Path
+  it "sends back a single market (happy)" do
+    market = create(:market)
+
+    get "/api/v0/markets/#{market.id}"
+
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+    market_data = parsed_json[:data]
+
+    expect(response.code).to eq("200")
+    expect(response).to be_successful
+
+    expect(market_data[:attributes]).to have_key(:name)
+    expect(market_data[:attributes]).to have_key(:street)
+    expect(market_data[:attributes]).to have_key(:city)
+    expect(market_data[:attributes]).to have_key(:county)
+    expect(market_data[:attributes]).to have_key(:state)
+    expect(market_data[:attributes]).to have_key(:zip)
+    expect(market_data[:attributes]).to have_key(:lat)
+    expect(market_data[:attributes]).to have_key(:lon)
+    expect(market_data[:attributes]).to have_key(:vendor_count)
+    expect(market_data[:id]).to be_a (String)     
+    expect(market_data[:attributes][:name]).to be_a(String)
+    expect(market_data[:attributes][:street]).to be_a(String)
+    expect(market_data[:attributes][:city]).to be_a(String)
+    expect(market_data[:attributes][:county]).to be_a(String)
+    expect(market_data[:attributes][:state]).to be_a(String)
+    expect(market_data[:attributes][:zip]).to be_a(String)
+    expect(market_data[:attributes][:lat]).to be_a(String)
+    expect(market_data[:attributes][:lon]).to be_a(String)
+    expect(market_data[:attributes][:vendor_count]).to be_a(Integer)
+  end
+
+  # User Story 2 - Sad Path
+  it "sends back a single market (sad)" do
+    get "/api/v0/markets/123123123123"
+
+    expect(response.code).to eq("404")
+    expect(response).not_to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=123123123123")
+  end
+
+  # User Story 3 - Happy Path
+  it "sends back a a list of vendors that belong to a market" do
+    market = create(:market)
+    vendors = create_list(:vendor, 5)
+    vendors.each do |vendor|
+      MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+    end
+
+    get "/api/v0/markets/#{market.id}/vendors"
+
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.code).to eq("200")
+    expect(response).to be_successful
+
+    # Add a lot more to the test!
+    #binding.pry
+  end
+
+  # User Story 3 - Sad Path
+  it "sends back a a list of vendors that belong to a market (sad)" do
+    get "/api/v0/markets/123123123123/vendors"
+
+    expect(response.code).to eq("404")
+    expect(response).not_to be_successful
+
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:status]).to eq("404")
+    expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=123123123123")
   end
 end
