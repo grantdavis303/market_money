11d4186b0898221f9e01dbb523730936148d1c54
Grant Davis <grantdavis303@gmail.com>
2024-05-14 15:09:08 -0600

    Create Vendor Factory

--- spec/factories/markets.rb
+++ spec/factories/markets.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+  # Market Factory
   factory :market do
     name { Faker::Company.name }
     street { Faker::Address.street_name }
@@ -8,5 +9,14 @@ FactoryBot.define do
     zip { Faker::Address.zip_code }
     lat { Faker::Address.latitude }
     lon { Faker::Address.longitude }
+  end
+
+  # Vendor Factory
+  factory :vendor do
+    name { Faker::Name.first_name }
+    description { Faker::Lorem.paragraph }
+    contact_name { Faker::Company.name }
+    contact_phone { Faker::PhoneNumber.phone_number }
+    credit_accepted { Faker::Boolean.boolean }
   end
 end
