e1a4c51ef246f67f5377fbdc5631f969070f5642
Grant Davis <grantdavis303@gmail.com>
2024-05-15 15:38:13 -0600

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index], controller: :market_vendors
       end
 
-      resources :vendors, only: [:show]
+      resources :vendors, only: [:show, :create, :update, :destroy]
     end
   end
 end
