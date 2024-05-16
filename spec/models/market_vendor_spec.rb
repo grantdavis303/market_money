a24c90ba048b660a19c5bb762660a5225879fe31
Grant Davis <grantdavis303@gmail.com>
2024-05-16 10:24:57 -0600

    Add Feature and Model Tests for US8

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe MarketVendor, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:market_id) }
+    it { should validate_presence_of(:vendor_id) }
+    #it { should validate_presence_of(:already_exists).on(:create) }
+  end
+
   describe "relationships" do
     it { should belong_to(:market) }
     it { should belong_to(:vendor) }
--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
- 
-describe 'APi marketvendor' do
-  #user story 8
-  it "creates a market vendor (happy)" do
+
+describe "MarketVendors API" do
+
+  # User Story 8 - Happy Path - Successful Create
+  it "creates a market vendor (happy 201)" do
     market = create(:market)
     vendor = create(:vendor)
     market_vendor_params = ({
@@ -20,8 +21,8 @@ describe 'APi marketvendor' do
     expect(data[:message]).to eq("Successfully added vendor to market")
   end
 
-  #user story 8
-  it "creates a market vendor (sad)" do
+  # User Story 8 - Sad Path 1 - Validation faiiled: Market must exist
+  it "doesn't create a market vendor (sad 404)" do
     market = create(:market)
     vendor = create(:vendor)
     market_vendor_params = ({
@@ -41,7 +42,8 @@ describe 'APi marketvendor' do
     expect(data[:errors].first[:detail]).to eq("Validation failed: Market must exist")
   end
 
-  it "creates a market vendor (sad)" do
+  # User Story 8 - Sad Path 2 - Validation failed: Market vendor asociation between market with market_id and vendor_id already exists
+  it "doesn't create a market vendor (sad 422)" do
     market = create(:market)
     vendor = create(:vendor)
     MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
@@ -52,13 +54,109 @@ describe 'APi marketvendor' do
     headers = {"CONTENT_TYPE" => "application/json"}
 
     post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
-    
+
     expect(response).not_to be_successful
     expect(response.status).to eq(422)
     
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(data[:errors]).to be_a(Array)
-    expect(data[:errors].first[:detail]).to eq("Validation failed: Market vendor asociation between market with market_id=70 and vendor_id=1150 already exists")
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Market vendor asociation between market with market_id=#{market.id} and vendor_id=#{vendor.id} already exists")
+  end
+
+  # User Story 8 - Sad Path 3 - Market can't be blank
+  it "doesn't create a market vendor (sad 400) - market can't be blank" do
+    market = create(:market)
+    vendor = create(:vendor)
+    market_vendor_params = ({
+      vendor_id: vendor.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(400)
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Market can't be blank, Market must exist")
+  end
+
+  # User Story 8 - Sad Path 34 - Vendor can't be blank
+  it "doesn't create a market vendor (sad 400) - vendor can't be blank" do
+    market = create(:market)
+    vendor = create(:vendor)
+    market_vendor_params = ({
+      market_id: market.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(400)
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Vendor can't be blank, Vendor must exist")
+  end
+
+  # User Story 8 - Sad Path 5 - Both can't be blank
+  it "doesn't create a market vendor (sad 400) - both can't be blank" do
+    market = create(:market)
+    vendor = create(:vendor)
+    market_vendor_params = ({})
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(400)
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+
+    expect(data[:errors]).to be_a(Array)
+    expect(data[:errors].first[:detail]).to eq("Validation failed: Market can't be blank, Vendor can't be blank, Market must exist, Vendor must exist")
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # User Story 9 - Happy Path
+  xit "deletes a market_vendor (happy)" do
+    market = create(:market)
+    vendor = create(:vendor)
+    MarketVendor.create!(market_id: market.id, vendor_id: vendor.id)
+
+    market_vendor_params = ({
+      market_id: market.id,
+      vendor_id: vendor.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    delete "/api/v0/market_vendors", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(204)
+
+    #data = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  # User Story 9 - Sad Path
+  xit "deletes a market_vendor (sad)" do
   end
 end
