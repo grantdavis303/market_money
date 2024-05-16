5661a88e27b2a7f87c50298a438223c4ec553b2b
Grant Davis <grantdavis303@gmail.com>
2024-05-16 13:28:50 -0600

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,11 @@ Rails.application.routes.draw do
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], controller: :market_vendors
       end
+
       resources :market_vendors, only: [:create]
+      delete "market_vendors", to: "market_vendors#destroy"
+
       resources :vendors, only: [:show, :create, :update, :destroy]
     end
   end
-
-  delete "/api/v0/market_vendors", to: "api/v0/market_vendors#destroy"
 end
