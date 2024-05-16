3b5a27b1ae8bd1a86125d891981edd321616b936
Grant Davis <grantdavis303@gmail.com>
2024-05-16 10:23:44 -0600

    Add MarketVendor Methods and Validations

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,14 +1,19 @@
 class MarketVendor < ApplicationRecord
+  validates_presence_of :market_id,
+                        :vendor_id
+
+  validate :already_exists, on: :create
+  
   belongs_to :market
   belongs_to :vendor
 
-  validate :already_exists, on: :create
+  def self.already_exists(id_1, id_2)
+    where(market_id: id_1, vendor_id: id_2)
+  end
 
   def already_exists
-    
-    require 'pry' ; binding.pry
-    # if market_id.exists? && vendor_id.exists?
-
-    # end
+    if MarketVendor.already_exists(market_id, vendor_id).empty? != true
+      errors.add(:base, "Market vendor asociation between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists")
+    end
   end
 end
