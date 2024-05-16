9a2f8f8ced2c9ff3083308d7bcd4d446b90e577f
Grant Davis <grantdavis303@gmail.com>
2024-05-16 10:23:08 -0600

    Refactor and Format Controllers for US8

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -10,20 +10,24 @@ class Api::V0::MarketVendorsController < ApplicationController
   def create
     market = params[:market_vendor][:market_id]
     vendor = params[:market_vendor][:vendor_id]
-    render json: MarketVendorSerializer.new(
-    MarketVendor.create!(market_id: market, vendor_id: vendor)).message, status: 201
-    
+    render json: MarketVendorSerializer.new(MarketVendor.create!(market_id: market, vendor_id: vendor)).message, status: 201
+  end
 
+  def destroy
+    #binding.pry
   end
 
   private def not_found_response(exception)
-    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-      .serialize_json, status: :not_found
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
   end
 
-  private def invalid_record(exception)
-    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json_detail, 
-    status: :not_found
+  private def invalid_record(exception) # Validation failed: Market must exist    
+    if exception.message.include?("already exists")
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 422)).serialize_json_detail, status: :unprocessable_entity
+    elsif exception.message.include?("can't be blank")
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400)).serialize_json_detail, status: :bad_request
+    else
+      render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json_detail, status: :not_found
+    end
   end
-
 end
--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -10,7 +10,6 @@ class Api::V0::MarketsController < ApplicationController
   end
 
   private def not_found_response(exception)
-    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-      .serialize_json, status: :not_found
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
   end
 end
