9651315c2d593d6836c10971de83aee9f0710d62
Grant Davis <grantdavis303@gmail.com>
2024-05-17 08:54:02 -0600

    Add Nearest ATMs Controller

---
+++ app/controllers/api/v0/nearest_atms_controller.rb
@@ -0,0 +1,40 @@
+class Api::V0::NearestAtmsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+  def index
+    market = Market.find(params[:id])
+    latitude = market.lat
+    longitude = market.lon
+    radius = 10000
+    limit = 12
+    poi_id = 7397 # Found with api.tomtom.com/search/2/poiCategories.json?
+
+    api_connection = Faraday.new(url: "https://api.tomtom.com") do |faraday|
+      faraday.headers["key"] = Rails.application.credentials.tomtom[:key]
+    end
+
+    api_response = api_connection.get("https://api.tomtom.com/search/2/nearbySearch/.json?lat=#{latitude}&lon=#{longitude}&limit=#{limit}&radius=#{radius}&categorySet=#{poi_id}&view=Unified&relatedPois=off&key=#{Rails.application.credentials.tomtom[:key]}")
+    
+    formatted_json = JSON.parse(api_response.body, symbolize_names: true)
+    query_results = formatted_json[:results]
+
+    @new_atm_objects = Array.new
+    query_results.each do |query_result|
+      atm_data = {
+        name: query_result[:poi][:name],
+        address: query_result[:address][:freeformAddress],
+        lat: query_result[:position][:lat],
+        lon: query_result[:position][:lat],
+        distance: query_result[:dist]
+      }
+      new_machine = Atm.new(atm_data)
+      @new_atm_objects << new_machine
+    end
+
+    render json: AtmSerializer.new.serialize_json(@new_atm_objects), status: 200
+  end
+
+  private def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
+  end
+end
